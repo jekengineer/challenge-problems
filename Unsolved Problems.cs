bc3c520cdcd11463dd5b19d0ea34ccc3844c9c75
James Kennedy <49706240+jekengineer@users.noreply.github.com>
2019-04-30 23:24:51 -0400

    Create Unsolved Problems.cs
    Initial Commit, added 3 problems to this

---
+++ Unsolved Problems.cs
@@ -0,0 +1,26 @@
+// This is a list of unsolved Problems
+
+// In Python
+// Create a way to receive the results of morse code, but also be able to give possible results 
+//      if the morse code has ?, where the . or - is not necessarily heard. This translator should be
+//      used for any amount of characters up to a reasonable amount.
+//      example would be decodeMorse('.... . -.--   .--- ..- -.. .') -> this returns HEY JUDE
+//      example would be decodeMorse('?... . -.--   .--- ..- -.. .') -> this returns either HEY JUDE or VEY JUDE
+//      for robustness let's assume you could give the user the top 100 possibilities, if there ever are that many.
+
+
+// In C#
+// Create a way for you to check between two dates WITHOUT using a DateTime object.
+
+
+// In C++
+// Ther are 3 parameters for this function. 
+//    n represents a count. 
+//    n_multiple represents a number where you check to see if it is a multiple of the count
+//    n_equal represents a number where you check to see if it equals the count
+//    Create a way to count to 1 to n without using a loop. 
+//    Check to see if it is a multiple of n, and if so skip some counting to where your counter is now equal to counter + n_multiple. 
+//    Check to see if n equals n_equal and if it does then end the count immediately
+//    example call count_to_n(n, n_multiple, n_equal) should return 1 to n with the checks so if you put in
+//    count_to_n(5, 0, 3) then the results would be 1, 2, 3
+//    count_to_n(5, 1, 999) then the results would be 1, 3, 5
